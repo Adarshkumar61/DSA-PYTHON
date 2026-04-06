83c50a3e9a69502448d69c1bea3cd0bff6dad789
Adarshkumar61 <adarshb2k61@gmail.com>
2026-04-06 12:15:57 +0530

    updated

--- data_structure_algorithm/basics/first.py
+++ data_structure_algorithm/basics/first.py
@@ -57,7 +57,7 @@
 #     def __init__(self, name , age):
 #         self.name = name 
 #         self.age = age
-# s1 = student('adarsh', 19)
+# s1 = student('adarsh', 19) 
 # print(s1.name)
 # print(s1.age)
 
