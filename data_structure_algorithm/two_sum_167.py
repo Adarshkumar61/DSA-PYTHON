053b7460cd09e3dfabff7ba643208eb4084977b4
Adarshkumar61 <adarshb2k61@gmail.com>
2026-06-23 19:29:25 +0530

    updated

--- data_structure_algorithm/two_sum_167.py
+++ data_structure_algorithm/two_sum_167.py
@@ -1,3 +1,14 @@
 #when array sorted
 
-def 
+def two_sum(arr, target):
+    left =0
+    right = len(arr) -1
+    
+    while left < right:
+        curr_sum = arr[left] +arr[right]
+        if curr_sum > target:
+            right-=1
+        elif curr_sum < target:
+            left+=1
+        else:
+            return [left, right]
