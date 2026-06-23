4876aa27c59438361f7e1b6eac93e590ffff9d3f
Adarshkumar61 <adarshb2k61@gmail.com>
2026-06-23 19:33:40 +0530

    completed

--- data_structure_algorithm/two_sum_167.py
+++ data_structure_algorithm/two_sum_167.py
@@ -12,3 +12,38 @@ def two_sum(arr, target):
             left+=1
         else:
             return [left, right]
+ 
+arr = [2, 7, 11, 15]
+target = 9
+# print(two_sum(arr, target))
+r = two_sum(arr, target)
+print(r)
+print(arr[r[0]], arr[r[1]])
+"""
+explanation :When:
+
+left == right
+
+you are pointing to the SAME element.
+
+Example:
+
+[2,7,11,15]
+    ↑
+   L,R
+
+Now if you calculate:
+
+arr[left] + arr[right]
+
+you're doing:
+
+7 + 7
+
+using the same element twice.
+
+Most Two Sum problems don't allow that.
+
+Therefore we stop at:
+
+while left < right """
