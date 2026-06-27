55856eeac12d91935f3a61d6d566a40dc5907e5f
Adarshkumar61 <adarshb2k61@gmail.com>
2026-06-27 19:59:22 +0530

    updated

--- data_structure_algorithm/two_pointer/movezeroes.py
+++ data_structure_algorithm/two_pointer/movezeroes.py
@@ -7,3 +7,21 @@
 #     return arr
 # arr = [1, 0, 3, 0, 6, 3, 0]
 # print(movezero(arr))
+
+
+
+def movezeroes(arr):
+    k = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[k] = arr[i]
+            k+=1
+    return k
+    # while k< len(arr):
+    #     arr[k] = 0
+    #     k+=1
+    # return arr
+
+arr = [1, 0, 3, 0, 6, 3, 0]
+r= movezeroes(arr)
+print(arr[:r])
