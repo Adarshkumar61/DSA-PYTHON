102037ab4f94bebab0a2c295b592e94dd296a063
Adarshkumar61 <adarshb2k61@gmail.com>
2026-07-07 20:14:10 +0530

    updated

--- data_structure_algorithm/arrays/prefix_sum.py
+++ data_structure_algorithm/arrays/prefix_sum.py
@@ -1,11 +1,11 @@
-# def prefix_sum(nums):
-#     prefix = [0] * len(nums)
-#     prefix[0] = nums[0]
-#     for i in range(1, len(nums)):
-#         prefix[i] = prefix[i-1] + nums[i]
-#     return sum(prefix[1:4])
-# nums = [5,10,15,20]
-# print(prefix_sum(nums))
+def prefix_sum(nums):
+    prefix = [0] * len(nums)
+    prefix[0] = nums[0]
+    for i in range(1, len(nums)):
+        prefix[i] = prefix[i-1] + nums[i]
+    return prefix
+nums = [5,10,15,20]
+print(prefix_sum(nums))
 
 
 
@@ -30,13 +30,13 @@
 
 
 
-def prefix_sum_of_index(nums, left_index, right_index):
-    prefix = [0] * len(nums)
-    prefix[0] = nums[0]
-    for i in range(1, len(nums)):
-        prefix[i] = prefix[i-1]+ nums[i]
-    if left_index == 0:
-        return prefix[right_index]
-    return prefix[right_index] - prefix[left_index-1]
-nums = [5,10,15,20]
-print(prefix_sum_of_index(nums, left_index= 0, right_index= 3))
+# def prefix_sum_of_index(nums, left_index, right_index):
+#     prefix = [0] * len(nums)
+#     prefix[0] = nums[0]
+#     for i in range(1, len(nums)):
+#         prefix[i] = prefix[i-1]+ nums[i]
+#     if left_index == 0:
+#         return prefix[right_index]
+#     return prefix[right_index] - prefix[left_index-1]
+# nums = [5,10,15,20]
+# print(prefix_sum_of_index(nums, left_index= 0, right_index= 3))
